202a3a13f0f349b608c6817037abaf3208ca5f54
ruraliz <ruraliz12@gmail.com>
2022-12-06 18:14:17 -0800

    add: base.html and crashed app

--- polls/tests.py
+++ polls/tests.py
@@ -46,7 +46,7 @@ class QuestionIndexViewTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
-            [question],
+            [question],  
         )
 
     def test_future_question(self):
@@ -69,7 +69,7 @@ class QuestionIndexViewTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
-            [question],
+            [question], 
         )
 
     def test_two_past_questions(self):
@@ -81,7 +81,7 @@ class QuestionIndexViewTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
-            [question2, question1],
+            [question2, question1],  
         )
 class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
@@ -90,7 +90,7 @@ class QuestionDetailViewTests(TestCase):
         returns a 404 not found.
         """
         future_question = create_question(question_text='Future question.', days=5)
-        url = reverse('polls:detail', args=(future_question.id,))
+        url = reverse('polls:detail', args=(future_question.id,))  
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
@@ -100,6 +100,6 @@ class QuestionDetailViewTests(TestCase):
         displays the question's text.
         """
         past_question = create_question(question_text='Past Question.', days=-5)
-        url = reverse('polls:detail', args=(past_question.id,))
+        url = reverse('polls:detail', args=(past_question.id,)) 
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
--- polls/views.py
+++ polls/views.py
@@ -37,7 +37,7 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice']) 
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
@@ -50,4 +50,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) 
