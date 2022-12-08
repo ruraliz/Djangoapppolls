8910b84bcd9a4e069381b8406be02aa6f84a4554
ruraliz <ruraliz12@gmail.com>
2022-12-07 18:37:28 -0800

    fixed: app working now

--- polls/admin.py
+++ polls/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
@@ -15,8 +16,8 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    # list_display = ('question_text', 'pub_date', 'was_published_recently')
-    # list_filter = ['pub_date']
-    search_fields = ['question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_test']
 
 admin.site.register(Question, QuestionAdmin)
--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-
+from django.views.generic.edit import CreateView
 from .models import Choice, Question
 # Create your views here.
 
@@ -14,32 +14,35 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
+        """Return the last five published questions."""
         return Question.objects.filter(
-        pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+            pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
-     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
         return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+class QuestionCreate(CreateView):
+  model = Question
+  fields = '__all__'
+  success_url = 'polls'
+
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice']) 
+    try: 
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
@@ -47,7 +50,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) 
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
